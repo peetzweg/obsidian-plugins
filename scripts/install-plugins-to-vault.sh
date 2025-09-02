aa30d838133089d179ad0bdba0ab44f77161438c
peetzweg <phil.czek@gmail.com>
2025-09-02 15:53:43 +0200

    make install script able to install in arbitrary vaults

--- scripts/install-plugins-to-vault.sh
+++ scripts/install-plugins-to-vault.sh
@@ -4,9 +4,16 @@ set -e
 echo $CWD
 current_dir=$(pwd)
 echo $current_dir
-
 plugins_dir="$current_dir/plugins"
-vault_dir="$current_dir/vault"
+
+if [ -n "$1" ]; then
+	vault_dir="$1"
+	echo "Using vault directory: $vault_dir"
+	vault_dir=$(realpath $vault_dir)
+else
+	vault_dir="$current_dir/vault"
+	echo "Using default vault directory: $vault_dir"
+fi
 vault_plugin_dir="$vault_dir/.obsidian/plugins"
 for plugin in "$plugins_dir"/*; do
 	echo "Installing '$(basename $plugin)' to '$(basename $vault_dir)'"
