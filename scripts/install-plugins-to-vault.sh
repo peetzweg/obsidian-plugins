2c857725abaebb1f45a8887cd0bba5322b72bbd4
pczek <839848+peetzweg@users.noreply.github.com>
2025-09-09 07:41:33 +0200

    makes install script more robust

--- scripts/install-plugins-to-vault.sh
+++ scripts/install-plugins-to-vault.sh
@@ -7,18 +7,18 @@ echo $current_dir
 plugins_dir="$current_dir/plugins"
 
 if [ -n "$1" ]; then
-	vault_dir="$1"
+	vault_dir=$(realpath "$1")
 	echo "Using vault directory: $vault_dir"
-	vault_dir=$(realpath $vault_dir)
 else
 	vault_dir="$current_dir/vault"
 	echo "Using default vault directory: $vault_dir"
 fi
+
 vault_plugin_dir="$vault_dir/.obsidian/plugins"
 for plugin in "$plugins_dir"/*; do
-	echo "Installing '$(basename $plugin)' to '$(basename $vault_dir)'"
-	if [ -d "$vault_plugin_dir/$(basename $plugin)" ]; then
-		rm -r "$vault_plugin_dir/$(basename $plugin)"
+	echo "Installing '$(basename "$plugin")' to '$(basename "$vault_dir")'"
+	if [ -e "$vault_plugin_dir/$(basename "$plugin")" ] || [ -L "$vault_plugin_dir/$(basename "$plugin")" ]; then
+		rm -rf "$vault_plugin_dir/$(basename "$plugin")"
 	fi
 	ln -s "$plugin" "$vault_plugin_dir/"
 done
